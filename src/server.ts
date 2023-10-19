88bed1b6c0bc2664d2624479022e5423fc92d7b6
ToufiqAnim <toufiq9610@gmail.com>
2023-10-19 22:59:23 +0600

    code edited

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
-import { errorlogger, logger } from './shared/logger';
 
 async function bootstrap() {
   mongoose
@@ -10,20 +9,20 @@ async function bootstrap() {
     .then(() => console.log('Database connceted successfully ✅'))
     .catch(err => console.log(`Unable to connect MongoDB ❌ ${err}`));
   const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
+    console.log(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
     if (server) {
       server.close(() => {
-        logger.info('Server closed');
+        console.log('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
+    console.log(error);
     exitHandler();
   };
 
@@ -31,7 +30,7 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
+    console.log('SIGTERM received');
     if (server) {
       server.close();
     }
