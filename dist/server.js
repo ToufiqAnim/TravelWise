938ee70d78c2a94675696f587eef583eae2df2aa
ToufiqAnim <toufiq9610@gmail.com>
2023-10-19 23:03:15 +0600

    code edited

--- dist/server.js
+++ dist/server.js
@@ -15,7 +15,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
-const logger_1 = require("./shared/logger");
 function bootstrap() {
     return __awaiter(this, void 0, void 0, function* () {
         mongoose_1.default
@@ -23,24 +22,24 @@ function bootstrap() {
             .then(() => console.log('Database connceted successfully ✅'))
             .catch(err => console.log(`Unable to connect MongoDB ❌ ${err}`));
         const server = app_1.default.listen(config_1.default.port, () => {
-            logger_1.logger.info(`Server running on port ${config_1.default.port}`);
+            console.log(`Server running on port ${config_1.default.port}`);
         });
         const exitHandler = () => {
             if (server) {
                 server.close(() => {
-                    logger_1.logger.info('Server closed');
+                    console.log('Server closed');
                 });
             }
             process.exit(1);
         };
         const unexpectedErrorHandler = (error) => {
-            logger_1.errorlogger.error(error);
+            console.log(error);
             exitHandler();
         };
         process.on('uncaughtException', unexpectedErrorHandler);
         process.on('unhandledRejection', unexpectedErrorHandler);
         process.on('SIGTERM', () => {
-            logger_1.logger.info('SIGTERM received');
+            console.log('SIGTERM received');
             if (server) {
                 server.close();
             }
